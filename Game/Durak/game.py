ba0a6559b223f38e79748025c83d6c671935f400
Andrew <zaharowanrew@gmail.com>
2024-07-09 16:32:29 +0400

    make a cards in strings

--- Game/Durak/game.py
+++ Game/Durak/game.py
@@ -4,57 +4,138 @@ import math
 from .strings import *
 
 class Card():
-    power:int # Сила карты от 6 до 14
-    suit:str # Масть. Строки , определяющие Масти перечислены в Durak/strings.py
+    def __init__(self, power: int, suit: str):
+        self.power = power  # Сила карты от 6 до 14
+        self.suit = suit  # Масть. Строки, определяющие масти, перечислены в Durak/strings.py
 
 class Player():
-    cards:list[Card]
+    def __init__(self):
+        self.cards = []  # Список карт игрока
 
 class Durak():
-    player1:Player
-    player2:Player
+    #Сделать ПРОВЕРКУ того, может ли игрок сделать такой ход
+    #Сделать создание колоды
+    # Ход и конфигурация attaked / defended меняется при поддтвердении атакующего игрока 
+    def __init__(self):
+        self.players = [Player(), Player()]
+        self.turn = 1 # Определяет чей сейчас ход - первого или второго игрока (по умолчанию первого)
 
-    cards:set[Card]
-    trump_card:Card#Карта которая является козырем
+        self.deck = set()
+        self.trump_card = None  # Карта, которая является козырем
+        self.attacking_player = 1 # Игрок, который в данный момент является атакующим (по умолчанию первый)
+        self.field = []  # Игровое поле
+        self.card_pool = [] # Карты , которые уже покрыты, но все еще остаются на поле
 
-    attacing_player:int#0 или 1 в зависимости от того, какой игрок защищается ("кроется")
-    field:list[Card]#Игровое поле
+    
+
+
 
     # Подкинуть карту.
-    # idx_cards_for_turn - массив индексов тех карт игрока ,которыми он ходит
-    def to_throw_in(self, idx_cards_for_turn :list[int]):
+    def to_throw_in(self, player_card_for_turn:int):
 
-        if(len(idx_cards_for_turn) == 0):
-            raise("length of a cards can't be a zero")
+        card_for_turn = self.players[self.attacking_player].cards[player_card_for_turn]
+
+        # хватит ли противнику карт, чтобы покрыться
+        if(self.attacking_player == 1): defending_player = 0
+        else: defending_player = 1
+        if(len(self.players[defending_player].cards) < len(self.field) + 1):
+            raise ERRORS[4]
+
+           
+        # Есть ли в card_pool карта, имеющая равную актакующей карте силу
+        if(len(self.card_pool) != 0):
+            if(not any(card.power == card_for_turn.power for card in self.card_pool)):
+                raise ERRORS[2]
+
+        # Если на поле карт несколько, то имеют ли они силу, равную атакующей карте, 
+        # тоесть имеют ли карты, подкинутые одновременно равную силу 
+        if(len(self.field) != 0):
+            if(not any(card.power == card_for_turn.power for card in self.field)):
+                raise ERRORS[3]
         
 
-        if(self.attacing_player == 1):
-            player = self.player1
-        elif(self.attacing_player == 2):
-            player = self.player2
+        # Добавление карты на поле
+        self.field.append(card_for_turn)
 
-
-        cards_for_turn = []
-        for card_idx in idx_cards_for_turn:
-            cards_for_turn.append(player.cards[card_idx])
-        
-        #Сделать проверку на то , что у противника хватит карт, чтобы покрыться
-
-        # Проверка, чтобы карты выброшенные игроком (если их несколько) имели одинаковую силу
-        card_power = cards_for_turn[0].power
-        for i in range(1, len(cards_for_turn)):
-            if(card_power != cards_for_turn[i].power):
-                return ERRORS["1"]
-            
-
-        for card in cards_for_turn:
-            self.field.append(card)
+        # Удаление карты из колоды игрока
+        self.players[self.attacking_player].cards.pop(player_card_for_turn)
 
         return None
     
-    # Покрыть карты.  Сделать функцию поступательной(обрабатывать по одной покрытой карте)!!!!!!!!!
-    def to_cover(self, ):
-        pass
+
+
+    # Покрыть карту.  Сделать функцию поступательной(обрабатывать по одной покрытой карте)!!!!!!!!!
+    def to_cover(self, player_attacking_cart:int, field_attacked_card:int):
+
+        attacking_cart = self.players[self.attacking_player].cards[player_attacking_cart]
+        attacked_card = self.field[field_attacked_card]
+
+        if(not (((attacking_cart.power > attacked_card.power) and attacking_cart.suit == attacked_card.suit) or \
+        attacking_cart.suit == self.trump_card.suit)):
+            raise(f"It is not possible to cover card {attacked_card} with card {attacked_card}")
+        
+        self.field.pop(field_attacked_card)
+        self.card_pool.append(attacked_card)
+        self.card_pool.append(attacking_cart)
+        self.players[self.attacking_player].cards.pop(player_attacking_cart)
+
+
+
+    # Подтвердить ход и проверить победу в игре
+    def confirm(self):
+        self.card_pool = []
+
+        if(len(self.deck) == 0 and len(self.players[self.turn].cards) == 0):
+            return WIN
+
+        self.give_cards(self.turn)
+
+
+    # Дать из колоды карты игроку
+    def give_cards(self, player_number:int):
+        while(   (len(self.players[player_number].cards) < 6) and (len(self.deck) != 0)    ):
+            self.players[player_number].cards.append(self.deck.pop())
+
+
+        
+    def can_cover(self, player_attacking_cart:int, field_attacked_card:int) -> bool:
+
+        attacking_cart = self.players[self.attacking_player].cards[player_attacking_cart]
+        attacked_card = self.field[field_attacked_card]
+
+        if(not (((attacking_cart.power > attacked_card.power) and attacking_cart.suit == attacked_card.suit) or \
+        attacking_cart.suit == self.trump_card.suit)):
+            return ERRORS[5]
+        
+        return True
+        
+
+
+
+    def can_throw_in(self, player_card_for_turn:int) -> bool:
+
+        card_for_turn = self.players[self.attacking_player].cards[player_card_for_turn]
+
+        # хватит ли противнику карт, чтобы покрыться
+        if(self.attacking_player == 1): defending_player = 0
+        else: defending_player = 1
+        if(len(self.players[defending_player].cards) < len(self.field) + 1):
+            raise ERRORS[4]
+
+           
+        # Есть ли в card_pool карта, имеющая равную актакующей карте силу
+        if(len(self.card_pool) != 0):
+            if(not any(card.power == card_for_turn.power for card in self.card_pool)):
+                raise ERRORS[2]
+
+        # Если на поле карт несколько, то имеют ли они силу, равную атакующей карте, 
+        # тоесть имеют ли карты, подкинутые одновременно равную силу 
+        if(len(self.field) != 0):
+            if(not any(card.power == card_for_turn.power for card in self.field)):
+                raise ERRORS[3]
+
+
+        
         
             
 
