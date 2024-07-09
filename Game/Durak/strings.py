6c67df91f5cc0c972f2c5f30f0aab8bf4bd93bca
Andrew <zaharowanrew@gmail.com>
2024-07-09 16:39:09 +0400

    Halp commit

--- Game/Durak/strings.py
+++ Game/Durak/strings.py
@@ -5,7 +5,7 @@ DIAMONTS_SUIT = "♦️"
 CLULBS_SUIT = "♣️"
 SPADES_SUIT = "♠️"
 
-#Errors:
+# Errors:
 
 ERRORS = {
     1 : "Ход несколькими картами возможен только если эти карты имеют одинаковую силу",
